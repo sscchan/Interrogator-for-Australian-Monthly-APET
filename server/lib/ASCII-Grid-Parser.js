20c6bf377b5f0c3cbe06832b03525c23049cc169
sscchan <stephen.s.c.chan@gmail.com>
2017-01-23 12:08:49 -0800

    add Grid Reference parsing

--- server/lib/ASCII-Grid-Parser.js
+++ server/lib/ASCII-Grid-Parser.js
@@ -1,8 +1,34 @@
 var fs = require('fs');
 
 function parseASCIIGrid(filepath) {
-  var fileContent = fs.readFileSync(filepath, 'utf8')
+  var gridReference = {};
+  var gridData = {};
+
+
+  var fileContent = fs.readFileSync(filepath, 'utf8');
+
+  // Remove any \r in case new line is representd by \r\n
+  fileContent = fileContent.replace('\r', '');
+
+  // Split file into lines
+  var fileLines = fileContent.split('\n');
+  // console.log(fileLines);
+
+  // Parse first 6 lines, which is the ASCII gird reference data
+  for (let i = 0; i < 6; i++) {
+    let line = fileLines[i];
+    let tokenValues = line.split(/\s+/);
+    let key = tokenValues[0];
+    let value = tokenValues[1];
+
+    gridReference[key] = value;
+  }
+
+  // Parse the rest of the data, and put them into gridData with key as the coorindate
+
+
+  console.log(JSON.stringify(gridReference));
   return fileContent;
-};
+}
 
 module.exports = parseASCIIGrid;
